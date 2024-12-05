19a9adb9bcda5e3f8f05de01d515ce417c14b057
RetroCoder <79114787+RetroCoder13@users.noreply.github.com>
2024-12-05 21:56:09 +0000

    Fixed styling issue of menu bar and also removes menu bar when the user scrolls

--- seven-wonders/menu.js
+++ seven-wonders/menu.js
@@ -1,8 +1,20 @@
 var menuButton = document.querySelector("#menuButton img")
 var menu = document.querySelector("#menu")
 
+menu.style.height = `calc(${document.body.offsetHeight}px - 30px`
+
 var toggle = false
 menuButton.onclick = function(e){
+    toggleMenu()
+}
+
+document.onscroll = function(){
+    if(toggle){
+        toggleMenu()
+    }
+}
+
+function toggleMenu(){
     toggle = !toggle
     if(toggle){
         // menu.style.animation = "pop-in .5s forwards"
