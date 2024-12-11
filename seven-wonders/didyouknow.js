9f3a4287cf8ea37f31d29a69a92a9d972e90189f
RetroCoder <79114787+RetroCoder13@users.noreply.github.com>
2024-12-11 10:13:37 +0000

    Added functionality to change text in dialogue box

--- seven-wonders/didyouknow.js
+++ seven-wonders/didyouknow.js
@@ -7,3 +7,12 @@ function closeDialogue(element){
     let textElement = element.children[0]
     textElement.style.left = 'calc(100% - 50px)'
 }
+
+facts = [
+    "Did you know?"
+]
+
+window.onload = function(){
+    let textElement = document.querySelector('#did-you-know #text')
+    textElement.innerHTML = facts[0]
+}
