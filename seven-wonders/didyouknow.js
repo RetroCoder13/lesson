719654f2169a286e63d1ddd92985dc5e3b80db39
RetroCoder <79114787+RetroCoder13@users.noreply.github.com>
2024-12-11 10:04:17 +0000

    Did you know box has functionality and now works. Updated icon colours aswell. Icon box can be smaller

---
+++ seven-wonders/didyouknow.js
@@ -0,0 +1,8 @@
+function openDialogue(element){
+    let textElement = element.children[0]
+    textElement.style.left = `calc(100% - 90px - ${textElement.offsetWidth}px)`
+}
+
+function closeDialogue(element){
+
+}
