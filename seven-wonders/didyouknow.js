345b0ad89f87e8b0b2ef43ae0361c41e628f1d6c
RetroCoder <79114787+RetroCoder13@users.noreply.github.com>
2024-12-11 10:10:11 +0000

    Updated info box

--- seven-wonders/didyouknow.js
+++ seven-wonders/didyouknow.js
@@ -1,8 +1,9 @@
 function openDialogue(element){
     let textElement = element.children[0]
-    textElement.style.left = `calc(100% - 90px - ${textElement.offsetWidth}px)`
+    textElement.style.left = `calc(100% - ${textElement.offsetWidth}px)`
 }
 
 function closeDialogue(element){
-
+    let textElement = element.children[0]
+    textElement.style.left = 'calc(100% - 50px)'
 }
