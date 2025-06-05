68b010510c87da7f93018b605c2e4caff959fb85
RetroCoder <79114787+RetroCoder13@users.noreply.github.com>
2025-06-05 12:50:19 +0000

    05-06-25

--- fruit-machine/main.js
+++ fruit-machine/main.js
@@ -2,6 +2,8 @@ var increment = 0
 var totalIncrement = 0
 var spinning = [true,true,true]
 var stopSpin = false
+var calculateScore = true
+var score = 1
 
 document.querySelectorAll("div.spinner>img").forEach(function(n){
     n.style.transform = `translateY(0px)`
@@ -24,6 +26,24 @@ function update(){
             })
         }
     })
+
+    if(JSON.stringify(spinning) == JSON.stringify([false,false,false]) && calculateScore){
+        calculateScore = false
+        let items = [itemVisible(0),itemVisible(1),itemVisible(2)]
+        if(items[0] == "bell" && items[1] == "bell" && items[2] == "bell"){
+            score += 5
+        } else if(items[0] == "skull" && items[1] == "skull" && items[2] == "skull"){
+            score = 0
+        } else if((items[0] == "skull" && items[1] == "skull") || (items[0] == "skull" || items[2] == "skull") && (items[1] == "skull" && items[2] == "skull")){
+            score -= 1
+        }  else if(items[0] == items[1] == items[2]){
+            score += 1
+        } else if(items[0] == items[1] || items[0] == items[2] || items[1] == items[2]){
+            score += .5
+        }
+        score = score.toFixed(1)
+        document.getElementById('score').innerHTML = `Score: ${score}`
+    }
 
     requestAnimationFrame(update)
 }
@@ -50,12 +70,25 @@ buttons[1].onclick = function(){
     totalIncrement = 0
     spinning = [true,true,true]
     stopSpin = false
+    calculateScore = true
+    score -= 0.2
     document.querySelectorAll("div.spinner").forEach(function(s){
         s.querySelectorAll("img").forEach(function(n){
             n.style.transform = `translateY(0px)`
         })
     })
     randomise()
+}
+
+function itemVisible(slot){
+    let images = document.getElementById(slot).querySelectorAll('img')
+    let containerRect = document.getElementById(slot).getBoundingClientRect()
+    for(let i=0;i<images.length;i++){
+        let imageRect = images[i].getBoundingClientRect()
+        if(imageRect.top >= containerRect.top && imageRect.left >= containerRect.left && imageRect.bottom <= containerRect.bottom && imageRect.right <= containerRect.right){
+            return images[i].name
+        }
+    }
 }
 
 randomise()
